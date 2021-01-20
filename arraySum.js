aa2ee6f32f033355c4eaa4cf7ece8263fde6caea
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-20 09:44:10 +0600

    final commit

---
+++ arraySum.js
@@ -0,0 +1,7 @@
+var numbers = [1, 3, 5, 7, 23, 16, 25];
+var sum = 0;
+for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    sum += element;
+}
+console.log("Total of numbers: ", sum);
